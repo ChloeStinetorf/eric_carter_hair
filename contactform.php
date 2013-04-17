c6ac7c95e8ff38f41fd75f0ce2321c3f38cf0879
Chloe <chloe.stinetorf@gmail.com>
2013-04-17 11:53:41 -0400

    initial commit of eric site after april 17 2013 updates

---
+++ contactform.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+Name: <?php echo $_POST['Name']; ?><br />
+Email: <?php echo $_POST['Email']; ?><br />
+Phone: <?php echo $_POST['Phone']; ?><br />
+Message:<br />
+<?php echo $_POST['Message']; ?>
+</body>
+</html>
